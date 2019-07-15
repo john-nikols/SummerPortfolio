39805539333ddda581f4275237cdc2a81cace89d
john-nikols <john.nikols123@gmail.com>
2019-07-14 19:49:50 -0600

    completed guess number game and added media & viewport to css

--- number.js
+++ number.js
@@ -1,11 +1,13 @@
-var randNum = Math.round( Math.random() * 201 ) - 100;
-function loadData(){
-    onload(randNum = Math.round( Math.random() * 201 ) - 100);
-}
+var randNum = 0;
+window.onload = function loadData(){
+    randNum = Math.floor(Math.random() * Math.floor(100))+ 1;
+    document.getElementById("myspan").innerHTML = randNum;
+};
 
 function sendData(form) {
     var guessNum = form.inputbox.value; 
    guessNum = parseFloat(guessNum);
+  
    // get value from text box
 
    if (guessNum < randNum) {
@@ -19,3 +21,4 @@ else {
     }       
 }
 }
+
